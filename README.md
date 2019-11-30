e662dd139e0d05d1c4b5ce4a895f150b695c8c45
Xavier Drdak <1198051+xdrdak@users.noreply.github.com>
2019-11-30 09:51:29 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Quick lazy NextJS setup
+
+Need to ship things fast and have the essentials already pre-configured in?
+
+Slap this template.
---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { NextPage } from 'next';
+
+const IndexPage: NextPage = () => {
+  return <div>Allo allo</div>;
+};
+
+export default IndexPage;
